5b58a58bb21f7f0d993da9ed8b0f1595232e5580
maulidakhairunisa <khairunisamaulida@gmail.com>
2026-02-07 07:25:32 +0700

    live session 7

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import {
-    createCategory,
-    getCategories,
-    getCategoryById,
-    updateCategory,
-    deleteCategory
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
 } from "../controllers/category.controller";
 import { upload } from "../middlewares/upload.middleware";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.post("/", authenticate,createCategory);
+router.post("/", authenticate, createCategory);
 router.get("/", getCategories);
 router.get("/:id", getCategoryById);
 router.put("/:id", authenticate, upload.single("image"), updateCategory);
